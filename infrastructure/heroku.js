a0cb4baa292a9da06b3edc2b9012f7670ca63044
ityvrich <yvonne.richa@schibsted.no>
2016-03-08 10:30:50 +0100

    prod object

--- infrastructure/heroku.js
+++ infrastructure/heroku.js
@@ -2,6 +2,26 @@ var heroin = require('heroin-js');
 
 var configurator = heroin(process.env.HEROKU_API_TOKEN, {debug: false});
 
-configurator.export('yvonnes-book-inventory-service').then(function(result) {
+/*configurator.export('yvonnes-book-inventory-service').then(function(result) {
     console.log(result);
-});
+});*/
+
+var prod = { name: 'yvonnes-book-inventory-service',
+    region: 'eu',
+    maintenance: false,
+    stack: 'cedar-14',
+    config_vars: { MONGOLAB_URI: 'mongodb://heroku_ngjsb33p:2b9jgi3tcadr458f5cook4lsvs@ds019488.mlab.com:19488/heroku_ngjsb33p' },
+    addons: { mongolab: { plan: 'mongolab:sandbox' } },
+    collaborators: [ 'tonje.sundby@schibsted.no', 'yvonne.richa@schibsted.no' ],
+    features:
+    { 'runtime-dyno-metadata': { enabled: false },
+        'log-runtime-metrics': { enabled: false },
+        'http-session-affinity': { enabled: false },
+        preboot: { enabled: false },
+        'http-shard-header': { enabled: false },
+        'http-end-to-end-continue': { enabled: false } },
+    formation: [ { process: 'web', quantity: 1, size: 'Free' } ],
+    log_drains: [],
+    domains: [ 'yvonnes-book-inventory-service.herokuapp.com' ] }
+
+configurator(prod);
